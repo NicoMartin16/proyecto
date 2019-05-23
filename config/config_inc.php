2b95221cf0c75605827f524925fef1e746a50397
Nicolas Martin <nicolasfelipemartin@hotmail.com>
2019-05-23 16:26:09 -0500

    Subiendo proyecto

---
+++ config/config_inc.php
@@ -0,0 +1,10 @@
+<?php
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'mantis';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'UTC';
+
+$g_crypto_master_salt     = 'xg2x8++FUsES/jNHB1QGfFUpKhZ+YPN1fSP9OFzTH/Y=';
